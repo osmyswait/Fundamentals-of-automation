dfbd37cda89b0e01a62cc2267ea94d21c42282b7
Vladimir Semenov <osmyswait@gmail.com>
2023-02-03 09:21:11 +0300

    add new branch JUnit4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,11 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class CashbackHackServiceTest {
+
+    @Test
+    public void remain() {
+    }
+}

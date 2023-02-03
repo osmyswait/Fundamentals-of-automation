b7a849af33b0fb1707e2b38b04bbe78b88cfd0ad
Vladimir Semenov <osmyswait@gmail.com>
2023-02-03 11:33:22 +0300

    add Tests for TestNG

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,12 +1,46 @@
 package ru.netology.service;
 
 import org.testng.annotations.Test;
+import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
 
     @Test
-    public void testRemain() {
+    public void ShouldRemainIfAmountUnderBoundary() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void ShouldRemainIfAmountMoreBoundary() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1500;
+
+        int actual = service.remain(amount);
+        int expected = 500;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void ShouldRemainIfAmountEqualsBoundary() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
     }
 }

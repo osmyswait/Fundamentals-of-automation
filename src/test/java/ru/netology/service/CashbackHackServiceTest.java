c9b23b1fa3dd9f392a04f3c0dadd6edee5bffbef
Vladimir Semenov <osmyswait@gmail.com>
2023-02-03 09:58:19 +0300

    add Tests for JUnit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -6,6 +6,38 @@ import static org.junit.Assert.*;
 public class CashbackHackServiceTest {
 
     @Test
-    public void remain() {
+    public void ShouldRemainIfAmountUnderBoundary() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 900;
+
+        int expected = 100;
+        int actual = service.remain(amount);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ShouldRemainIfAmountMoreBoundary() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1500;
+
+        int expected = 500;
+        int actual = service.remain(amount);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ShouldRemainIfAmountEqualsBoundary() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1000;
+
+        int expected = 0;
+        int actual = service.remain(amount);
+
+        assertEquals(expected, actual);
     }
 }

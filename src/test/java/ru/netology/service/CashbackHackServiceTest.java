3be74a2077a963e4c44fabf2a15b59b12f7ec923
Vladimir Semenov <osmyswait@gmail.com>
2023-02-03 09:13:28 +0300

    add new branch TestNG

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,12 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemain() {
+    }
+}
